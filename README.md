31ef0bf4c4dba08aecdd79df4f4b074c9cdd8bb7
daaspraveen <102827315+daaspraveen@users.noreply.github.com>
2023-01-14 17:38:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-oke4di1-100
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-oke4di)
---
+++ index.js
@@ -0,0 +1,13 @@
+for(let i=1; i<=100; i++){
+  if(i % 5 == 0 && i % 3 == 0){
+    console.log("amazon");
+    }
+  else if(i % 5 == 0){
+    console.log("facebook");
+    }
+  else if( i%3==0){
+    console.log("google");
+    }
+  else
+  {
+      console.log(i);
